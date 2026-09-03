22dbf5724d276dfba2856a0e6f9459f7b3fafc21
ganesh-khandekar <ganeshkhandekar092@gmail.com>
2026-09-03 20:13:28 +0530

    first commit

---
+++ curd-Operation/src/main/java/com/ganesh/curd_Operation/CurdOperationApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.curd_Operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdOperationApplication.class, args);
+	}
+
+}
